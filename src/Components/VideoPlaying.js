891a93b1894da248e4eece1ade144c9cbbaea233
kamol-nazarov <kamol.nazarov15@myhunter.cuny.edu>
2020-08-04 06:49:23 -0400

    a

--- src/Components/VideoPlaying.js
+++ src/Components/VideoPlaying.js
@@ -1,46 +1,26 @@
 import React, { Component, useState, useCallback } from 'react';
 // import ReactPlayer from 'react-player';
-import Cropper from 'react-easy-crop'
-
+import Cropper from 'react-easy-crop';
 
 const myVideo = '/sample-mp4-file.mp4';
 
-const [crop, setCrop] = useState({ x: 0, y: 0 })
-const [initialCroppedAreaPixels, setInitialCroppedAreaPixels] = useState(
-	undefined
-)
-const [zoom, setZoom] = useState(1)
+const [ crop, setCrop ] = useState({ x: 0, y: 0 });
+const [ initialCroppedAreaPixels, setInitialCroppedAreaPixels ] = useState(undefined);
+const [ zoom, setZoom ] = useState(1);
 
 class Video extends Component {
-
-
-
-  useEffect(() => {
-    const croppedAreaPixels = JSON.parse(
-      window.localStorage.getItem('croppedAreaPixels')
-    )
-    setInitialCroppedAreaPixels(croppedAreaPixels)
-  }, [])
-
-  const onCropComplete = useCallback((croppedArea, croppedAreaPixels) => {
-    console.log(croppedArea, croppedAreaPixels)
-    window.localStorage.setItem(
-      'croppedAreaPixels',
-      JSON.stringify(croppedAreaPixels)
-    )
-  }, [])
-	
 	render() {
-		return <Cropper
-		video={this.state.video}
-		crop={this.state.crop}
-		zoom={this.state.zoom}
-		aspect={this.state.aspect}
-		onCropChange={this.onCropChange}
-		onCropComplete={this.onCropComplete}
-		onZoomChange={this.onZoomChange}
-	/>
-		
+		return (
+			<Cropper
+				video={this.state.video}
+				crop={this.state.crop}
+				zoom={this.state.zoom}
+				aspect={this.state.aspect}
+				onCropChange={this.onCropChange}
+				onCropComplete={this.onCropComplete}
+				onZoomChange={this.onZoomChange}
+			/>
+		);
 	}
 }
 
